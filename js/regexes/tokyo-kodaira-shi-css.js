529d9fd99713a892a862e6b2c26b958b53b9699c
Yasutake Yohei <github@yasutakeyohei.com>
2020-03-06 17:44:46 +0900

    add css file

---
+++ js/regexes/tokyo-kodaira-shi-css.js
@@ -0,0 +1,21 @@
+export const regexes = `
+a {
+  font-size:8px;
+  text-decoration: none;
+}
+
+.a4 {
+  font-family: koruri;
+  width: 100%;
+  font-size:12px;
+  padding:10px;
+}
+
+.ikkatu {
+  display: inline-block;
+}
+
+.ikkatu th {
+  text-align: left;
+}
+`;
