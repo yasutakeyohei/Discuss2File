9e91e321b8b9298a32e27291415863c386c27083
Yasutake Yohei <github@yasutakeyohei.com>
2020-03-07 17:57:34 +0900

    add default css

--- js/regexes/tokyo-kodaira-shi-css.js
+++ js/regexes/tokyo-kodaira-shi-css.js
@@ -1,21 +1,32 @@
-export const regexes = `
+export const css = `
 a {
-  font-size:8px;
+  font-size: 8px;
   text-decoration: none;
 }
-
 .a4 {
-  font-family: koruri;
   width: 100%;
-  font-size:12px;
-  padding:10px;
+  font-size: 12px;
+  padding: 10px;
 }
-
 .ikkatu {
   display: inline-block;
 }
-
 .ikkatu th {
   text-align: left;
+}
+.date {
+  background-color: #195b94;
+  color: #fff;
+  padding: 5px;
+  font-weight: bold;
+}
+.open {
+  background-color: #c7e4f5;
+}
+.nittei {
+  color: blue;
+}
+.speak-start {
+  margin-top: 15px;
 }
 `;
