020001f68e7b182ae62f83498f660f5781f68522
Yasutake Yohei <github@yasutakeyohei.com>
2020-03-07 17:55:58 +0900

    add allowed tags and add styletag parser

--- js/htmlSanitizer.js
+++ js/htmlSanitizer.js
@@ -1,94 +1,110 @@
-const htmlSanitizer = () => {
-    const tagWhitelist = [
-        'A','ABBR',
-        'B', 'BLOCKQUOTE', 'BODY', 'BR',
-        'CENTER', 'CODE',
-        'DIV',
-        'EM',
-        'FONT',
-        'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HR',
-        'I', 'IMG',
-        'LABEL', 'LI',
-        'OL',
-        'P', 'PRE',
-        'SMALL', 'SOURCE', 'SPAN', 'STRONG',
-        'TABLE', 'TBODY', 'TR', 'TD', 'TH', 'THEAD',
-        'UL', 'U',
-        'VIDEO'
-    ];
-    
-    const attributeWhitelist = [
-        'align',
-        'color', 'class',
-        'height', 'href',
-        'src', 'style',
-        'target', 'title', 'type',
-        'width'
-    ];
+const tagWhitelist = [
+    'A','ABBR',
+    'B', 'BLOCKQUOTE', 'BODY', 'BR',
+    'CENTER', 'CODE',
+    'DIV',
+    'EM',
+    'FIELDSET', 'FONT',
+    'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HR',
+    'I', 'IMG',
+    'LABEL', 'LEGEND', 'LI',
+    'OL',
+    'P', 'PRE',
+    'SMALL', 'SOURCE', 'SPAN', 'STRONG', 'STYLE',
+    'TABLE', 'TBODY', 'TR', 'TD', 'TH', 'THEAD',
+    'UL', 'U'
+];
 
-    const cssWhitelist = [
-        'background-color',
-        'color',
-        'font-size',
-        'font-weight',
-        'text-align',
-        'text-decoration'
-    ];
+const attributeWhitelist = [
+    'align',
+    'color', 'class',
+    'height', 'href',
+    'src', 'style',
+    'target', 'title', 'type',
+    'width'
+];
 
-    const schemaWhiteList = ['http:', 'https:'];
-    
-    const sanitizeHtml = (input) => {
-        let iframe = document.createElement('iframe');
-		if(iframe['sandbox'] === undefined) {
-            alert('ブラウザが古いためiframeのsandboxモードが使えません。新しいブラウザを使用してください。');
-			return '';
-		}
-		iframe['sandbox'] = 'allow-same-origin';
-		iframe.style.display = 'none';
-        document.body.appendChild(iframe);
-		let iframedoc = iframe.contentDocument || iframe.contentWindow.document;
-		if(iframedoc.body == null) iframedoc.write("<body></body>"); // null in IE
-		iframedoc.body.innerHTML = input;
+const cssWhitelist = [
+    'background-color',
+    'color',
+    'display',
+    'font-size', 'font-weight', 'font-family',
+    'margin', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
+    'padding', 'padding-top', 'padding-right', 'padding-bottom', 'padding-left',
+    'text-align',
+    'text-decoration', 'text-decoration-line', 'text-decoration-style', 'text-decoration-color',
+    'width', 'height'
+];
 
-		const makeSanitizedCopy = (node) => {
-			if(node.nodeType === Node.TEXT_NODE) {
-				var newNode = node.cloneNode(true);
-			} else if(node.nodeType == Node.ELEMENT_NODE && (tagWhitelist.indexOf(node.tagName) > -1)) {
-                newNode = iframedoc.createElement(node.tagName);
+const schemaWhiteList = ['http:', 'https:'];
 
+export const sanitizeHtml = (input) => {
+    let iframe = document.createElement('iframe');
+    if(iframe['sandbox'] === undefined) {
+        alert('ブラウザが古いためiframeのsandboxモードが使えません。新しいブラウザを使用してください。');
+        return '';
+    }
+    iframe['sandbox'] = 'allow-same-origin';
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe);
+    let iframedoc = iframe.contentDocument || iframe.contentWindow.document;
+    if(iframedoc.body == null) iframedoc.write("<body></body>"); // null in IE
+    iframedoc.body.innerHTML = input;
+
+    const makeSanitizedCopy = (node) => {
+        if(node.nodeType === Node.TEXT_NODE) {
+            var newNode = node.cloneNode(true);
+        } else if(node.nodeType == Node.ELEMENT_NODE && (tagWhitelist.indexOf(node.tagName) > -1)) {
+            newNode = iframedoc.createElement(node.tagName);
+
+            if(node.tagName === "STYLE") {
+                let cssString = "";
+                for (const cssRule of node.sheet.cssRules) {
+                    cssString += cssRule.selectorText + " {";
+                    for (const styleName of cssRule.style) {
+                        console.log(styleName);
+                        if(cssWhitelist.indexOf(styleName) > -1) {
+                            cssString += styleName + ":" + cssRule.style.getPropertyValue(styleName) + ";";
+                            // bugがある？ので以下の方法は使えない。sheetがundefinedになる。
+                            // newNode.sheet.insertRule(cssRule.selectorText + "{" + styleName + ":" + cssRule.style.getPropertyValue(styleName) + ";}") 
+                        }
+                    }
+                    cssString += "}\n";
+                }
+                console.log(cssString);
+                newNode.innerHTML = cssString;
+            } else {
                 for (const attr of node.attributes) {
                     attributeLoop:
                     if(attributeWhitelist.indexOf(attr.name) > -1) {
-						if(attr.name === "style") {
-							for (const styleName of node.style) {
-								if(cssWhitelist.indexOf(styleName) > -1) {
+                        if(attr.name === "style") {
+                            for (const styleName of node.style) {
+                                if(cssWhitelist.indexOf(styleName) > -1) {
                                     newNode.style.setProperty(styleName, node.style.getPropertyValue(styleName));
                                 }
-							}
-						} else {
-							if (attr.name === "href" && attr.value.indexOf(":") > -1) {
+                            }
+                        } else {
+                            if (attr.name === "href" && attr.value.indexOf(":") > -1) {
                                 for (const schema of schemaWhiteList) {
-                                    if(attr.value.indexOf(schema) !== 0) continue attributeLoop;
+                                    if(attr.value.indexOf(schema) !== 0) break attributeLoop;
                                 }
-							}
-							newNode.setAttribute(attr.name, attr.value);
+                            }
+                            newNode.setAttribute(attr.name, attr.value);
                         }
                     }
                 }
 
-				for (const childNode of node.childNodes) {
-					let subCopy = makeSanitizedCopy(childNode);
-					newNode.appendChild(subCopy, false);
-				}
-			} else {
-				newNode = document.createDocumentFragment();
-			}
-			return newNode;
-		};
-
-		let resultElement = makeSanitizedCopy(iframedoc.body);
-		document.body.removeChild(iframe);
-		return resultElement.innerHTML
-    }
-
+                for (const childNode of node.childNodes) {
+                    let subCopy = makeSanitizedCopy(childNode);
+                    newNode.appendChild(subCopy, false);
+                }
+            }
+        } else {
+            newNode = document.createDocumentFragment();
+        }
+        return newNode;
+    };
+    let resultElement = makeSanitizedCopy(iframedoc.body);
+    document.body.removeChild(iframe);
+    return resultElement.innerHTML
 }
