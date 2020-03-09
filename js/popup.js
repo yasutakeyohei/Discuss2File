74ebe7435326ed25efee784e5e48e5c5f84f4ecf
Yasutake Yohei <github@yasutakeyohei.com>
2020-03-09 15:00:13 +0900

    add async sleep, loading icon, hankakuNumber, loading wait, link menu, update pageMode

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,10 @@
 let content = {}; // see singleMinuteParase or councilsParser
 let fontModule = null;
-let urlInfo = { cityName: "" }
+let urlInfo = { cityName: "", pageMode: -1 }
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const awaitParseSingleMinute = (mode) => {
   return new Promise(async (resolve) => {
@@ -17,7 +21,10 @@ const awaitParseSingleMinute = (mode) => {
       content = message.content;
       if(content.result === "singleParsed") {
         browser.runtime.onMessage.removeListener(listener);
-        $("#filename").val(content.filename);
+        if(urlInfo.pageMode === PAGE_MODE_SINGLE) {
+          $("#filename").val(content.filename);
+          hankakuNumber();
+        }
         resolve();
       }
     }
@@ -29,6 +36,7 @@ const awaitParseSingleMinute = (mode) => {
 const awaitParseCouncils = () => {
   return new Promise(async (resolve) => {
     var config = {
+      pageMode: urlInfo.pageMode,
       mode: MODE_COUNCILS_PARSE
     }
     await browser.tabs.executeScript(null, { code: 'var config = ' + JSON.stringify(config) });
@@ -36,6 +44,7 @@ const awaitParseCouncils = () => {
       content = message.content;
       if(content.result === "councilsParsed") {
         browser.runtime.onMessage.removeListener(listener);
+        $("#loading").hide();
 
         let currentGroup = "";
         for (const council of content.councils) {
@@ -43,13 +52,15 @@ const awaitParseCouncils = () => {
             $('#councils').append('<div class="group-title">' + council.group + '</div>');
             currentGroup = council.group;
           }
-          $('#councils').append('<label><input type="checkbox" name="councils" data-id="' + council.id + '" data-title="'+ council.title + '" />' + council.title + '</label><br />');
+          $('#councils').append('<label><input type="radio" name="councils" data-id="' + council.id + '" data-title="'+ council.title + '" />' + council.title + '</label><br />');
         }
         $("[name=councils]").change( async () => {
           //saveCouncilOptions();
           let selectedCouncils = [];
           $('[name="councils"]:checked').each((idx, elm) => {
             selectedCouncils.push({id: $(elm).attr("data-id"), title: $(elm).attr("data-title")});
+            $("#filename").val($(elm).attr("data-title"));
+            hankakuNumber();
           });
           await awaitParseSchedulesFromCouncils(selectedCouncils);
         });
@@ -58,6 +69,7 @@ const awaitParseCouncils = () => {
       }
     }
     browser.runtime.onMessage.addListener(listener);
+    $("#loading").show();
     browser.tabs.executeScript(null, { file: "js/content_scripts/councilsParser.js" });  
   });
 }
@@ -65,7 +77,8 @@ const awaitParseCouncils = () => {
 const awaitParseSchedulesFromCouncils = (councils) => {
   return new Promise(async (resolve) => {
     var config = {
-      mode: MODE_SCHEDULES_SHOW_AND_PARSE,
+      pageMode: urlInfo.pageMode,
+      mode: MODE_SCHEDULES_SHOW_AND_PARSE_IDS,
       councils: councils
     }
     await browser.tabs.executeScript(null, { code: 'var config = ' + JSON.stringify(config) });
@@ -73,6 +86,7 @@ const awaitParseSchedulesFromCouncils = (councils) => {
       content = message.content;
       if(content.result === "scheduleParsed") {
         browser.runtime.onMessage.removeListener(listener);
+        $("#loading").hide();
 
         const councilId = content.councilId;
         $('#schedules').html("");
@@ -81,15 +95,16 @@ const awaitParseSchedulesFromCouncils = (councils) => {
           for (const schedule of council.schedules) {
             $('#schedules').append('<label><input type="checkbox" name="schedules" data-council-id="' + council.id + '" data-schedule-id="' + schedule.id + '" checked="checked" />' + schedule.title + '</label><br />');
           }
-          if($('#schedules').text().length > 0) {
-            $("#schedules").show();
-          } else {
-            $("#schedules").hide();
-          }
+        }
+        if($('#schedules').text().length > 0) {
+          $("#schedules").show('fast');
+        } else {
+          $("#schedules").hide('fast');
         }
       }
     }
     browser.runtime.onMessage.addListener(listener);
+    $("#loading").show();
     browser.tabs.executeScript(null, { file: "js/content_scripts/councilsParser.js" });
   });
 }
@@ -97,23 +112,39 @@ const awaitParseSchedulesFromCouncils = (councils) => {
 const downloadFromSchedules = async () => {
   const baseUrl = "https://ssp.kaigiroku.net/tenant/" + urlInfo.cityName + "/SpMinuteView.html?";
   let idpairs = [];
+  let firstCouncilId = -1; //チェックされたうちの最初のcouncilIdを得る→そのIdのタイトルでファイル名を作成する
   $('input[name=schedules]:checked').each((idx, elm) => {
+    if(firstCouncilId === -1) firstCouncilId = $(elm).attr("data-council-id");
     idpairs.push({councilId: $(elm).attr("data-council-id"), scheduleId: $(elm).attr("data-schedule-id")});
   });
 
+  if(idpairs.length > 10) {
+    alert("サーバーへの負荷を考慮し、一度に選択できる項目を10個までに制限しております。");
+    $("#loading").hide();
+    return;
+  }
+
+  $("#loading").show();
+  let loopCount = 0;
   let multipleParsedContent = "";
-  for (const idpair of idpairs) {
+  for (let idpair of idpairs) {
+    if(loopCount++ != 0) {
+      let waitMSec = 1000 + loopCount * 500;
+      $("#loading > span").text(waitMSec/1000 + "秒待機（負荷軽減用）");
+      await sleep(waitMSec);
+    }
     let url = baseUrl + "council_id=" +  idpair.councilId + "&schedule_id=" + idpair.scheduleId;
     // executescript前にupdate（ページ遷移）がcompleteするのを待つ必要がある
     // でないと、ページ遷移前のページでscriptをexecuteすることになってしまう
     // https://stackoverflow.com/questions/4584517/chrome-tabs-problems-with-chrome-tabs-update-and-chrome-tabs-executescript
-    console.log("await");
     await awaitBrowserTabUpdate(url);
     await awaitParseSingleMinute(MODE_SINGLE_MINUTE_WAITLOAD_AND_PARSE);
     multipleParsedContent += content.parsedContent;
   }
+  $("#loading span").text("");
+  $("#loading").hide();
+
   downloadSingleMinuteHTML(multipleParsedContent);
-  console.log("dwonload");
 //  parseContent(id);
 }
 
@@ -172,15 +203,15 @@ const downloadSingleMinutePDF = async (parsedContent) => {
 
 const downloadSingleMinuteHTML = async (parsedContent) => {
   $("#replaceRegexErr").hide();
-  const filename = $("#filename").val() + ".html";
+  const filename = $("#filename").val();
 
   //const baseUrl = '<a href="https://ssp.kaigiroku.net/tenant/kodaira/$1">($2)</a>';
   const baseUrl = 'https://ssp.kaigiroku.net/tenant/' + urlInfo.cityName + '/';
 
-  const startTime = performance.now();
+  //const startTime = performance.now();
 
   let regexes = $("#replaceRegex").text().split(/\r?\n/g);
-  console.log(regexes);
+  //console.log(regexes);
 
   // regex parse
   let err = {};
@@ -217,7 +248,7 @@ const downloadSingleMinuteHTML = async (parsedContent) => {
       replace = replace.replace(/\{baseUrl\}/g, baseUrl);
 
       const r = new RegExp(regex, flag);
-      console.log(parsedContent);
+      //console.log(parsedContent);
       parsedContent = parsedContent.replace(r, replace);
     } else {
       err = { index: i + 1, message: "置換文字列が見つかりません" + replace.length };
@@ -242,29 +273,73 @@ const downloadSingleMinuteHTML = async (parsedContent) => {
 
 //  parsedContent = parsedContent.replace(/\<strong\>\<a href=\"(.*)\"\ target\=\"_self\"\>(.*)\<\/a\>\<\/strong\>/gm, hyperLink);
 
-  const endTime = performance.now(); // 終了時間
-  console.log(endTime - startTime);
+  //const endTime = performance.now(); // 終了時間
+  //console.log(endTime - startTime);
+  
+  const linkMenuCss = `
+  #linkMenu {display: inline-block;}
+  #linkMenu legend {font-size: 12px;}
+  #linkMenu ul {list-style:none;margin:0;padding:0;}
+  #linkMenu li.h2 {margin-left: 10px;font-size: 16px;}
+  #linkMenu li.h3 {margin-left: 20px;font-size: 16px;}
+  #linkMenu li.h4 {margin-left: 30px;font-size: 16px;}
+  #linkMenu li.h5 {margin-left: 40px;font-size: 16px;}
+  #linkMenu li.h6 {margin-left: 40px;font-size: 16px;}
+  #linkMenu li a{font-size: 12px;}
+  #linkMenu li.h1 a{font-size: 14px;}
+  #linkMenu li.h2 a{font-size: 13px;}
+  `;
 
-  const head = '<style type="text/css">\n' + $("#customCss").text() + '</style><div class="a4">\n';
+  const head = '<style type="text/css">\n' + $("#customCss").text() + linkMenuCss + '</style><div class="a4">\n';
   const foot = '</div>';
 
   parsedContent = head + parsedContent + foot;
 
   const mo = await import('./htmlSanitizer.js');
-  parsedContent = mo.sanitizeHtml(parsedContent);
+  let sanitizedContent = mo.sanitizeHtml(parsedContent);
 
+  //h1-h6に対してリンクメニューを作成
+  let sanitizedDom = $("<div>").append(sanitizedContent); //なぜかdivにつけないとDOM操作が正しく行えない。bodyもhtmlもダメ。
+  if($("#addMenu").is(":checked")) {
+    let linkMenuDiv = $("<fieldset>").attr("id", "linkMenu").append($("<legend>").text("自動作成目次"));
+    let indices = [];
+    let ulDom = $("<ul>");
+    $(sanitizedDom).find(':header').each((idx, elm) => {
+      let anchor = $('<a>');
+
+      const hIndex = parseInt(elm.nodeName.substring(1)) - 1;
+      if(indices[hIndex] === undefined) indices[hIndex] = 0;
+      indices[hIndex]++;
+
+      if($(elm).attr("id")) {
+        $(anchor).attr("href", '#' + $(elm).attr("id"));
+      } else {
+        const hid = (hIndex+1) + "-" + indices[hIndex];
+        $(elm).attr("id", hid);
+        $(anchor).attr("href", "#" + hid);
+      }
+      $(anchor).text($(elm).text());
+      $(ulDom).append($("<li>").addClass("h"+(hIndex+1)).html(anchor));
+    });
+    $(sanitizedDom).find("style").first().after($(linkMenuDiv).append(ulDom));
+    //console.log(, linkMenuDiv, $(sanitizedDom));
+  }
+  sanitizedContent = $(sanitizedDom).html();
+
+  sanitizedContent = `<!DOCTYPE HTML><html lang="jp"><head><title>${filename}</title></head><body>${sanitizedContent}</body></html>`;
+  //console.log(sanitizedContent);
 
   if($("[type='radio'][name='htmlSave'][value='save']").is(":checked")){
     var elm = document.createElement('a');
-    elm.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(parsedContent));
-    elm.setAttribute('download', filename);
+    elm.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(sanitizedContent));
+    elm.setAttribute('download', filename + ".html");
     elm.style.display = 'none';
     $("#downloadLink").append(elm);
     elm.click();
     $("#downloadLink").empty();
   } else {
     let w = window.open("");
-    w.document.write(parsedContent);
+    w.document.write(sanitizedContent);
     w.print();
     w.close();
   }
@@ -303,14 +378,32 @@ const loadOptions = async () => {
   }
 }
 
+const hankakuNumber = () => {
+  if($("#hankakuNumber").is(":checked")){
+    const name = $("#filename").val().replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
+      return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    });
+    //console.log(name);
+    $("#filename").val(name);
+  }
+}
+
 const main = async () => {
   await loadOptions();
   const tabs = await browser.tabs.query({'active': true, 'lastFocusedWindow': true});
   const url = tabs[0].url;
   urlInfo.cityName = url.match(/^.*tenant\/([^/]*)\/.*$/)[1];
-  let pageMode = url.match(/schedule_id/) ? "single" : "council";
-  if(url.match(/schedule-dialog/)) pageMode = "schedule";
 
+  if(url.match(/^.*\/SpTop.html.*/)){
+    urlInfo.pageMode = PAGE_MODE_TOP;
+  } else if(url.match(/#schedule-dialog/)) {
+    urlInfo.pageMode = PAGE_MODE_SCHEDULE_DIALOG;
+  } else if(url.match(/schedule_id\=/)) {
+    urlInfo.pageMode = PAGE_MODE_SINGLE;
+  } else if(url.match(/view_years\=/)) {
+    urlInfo.pageMode = PAGE_MODE_YEARLY;
+  }
+ 
   //toggler
   $('[data-role=toggler]').each((idx, elm) => {
     $(elm).on( 'click', () => {
@@ -346,9 +439,10 @@ const main = async () => {
     }
   });
 
-  $("[name=fileType], [name=txtCrlf], [name=htmlSave], [id=removeSpacesFromFilename]").change(() => {
+  $("[name=fileType], [name=txtCrlf], [name=htmlSave], [id=removeSpacesFromFilename], [id=hankakuNumber]").change(() => {
+    hankakuNumber();
     saveOptions();
-    if(pageMode === "single") awaitParseSingleMinute(MODE_SINGLE_MINUTE_PARSE);
+    if(urlInfo.pageMode === PAGE_MODE_SINGLE) awaitParseSingleMinute(MODE_SINGLE_MINUTE_PARSE);
   });
 
   $("textarea").on( 'keyup', () => {
@@ -361,11 +455,12 @@ const main = async () => {
 
   $('#download').click((e) => {
     e.preventDefault();
-    switch(pageMode) {
-      case "council":
+    switch(urlInfo.pageMode) {
+      case PAGE_MODE_TOP:
+      case PAGE_MODE_YEARLY:
         downloadFromSchedules();
         break;
-      case "single":
+      case PAGE_MODE_SINGLE:
         if($("[type='radio'][name='fileType'][value='html']").is(":checked")){
           downloadSingleMinuteHTML(content.parsedContent);
         } else {
@@ -377,12 +472,13 @@ const main = async () => {
     //window.close();
   });
 
-  switch(pageMode) {
-    case "council":
-      $("#councils").show();
+  switch(urlInfo.pageMode) {
+    case PAGE_MODE_TOP:
+    case PAGE_MODE_YEARLY:
+        $("#councils").show();
       awaitParseCouncils();
       break;
-    case "single":
+    case PAGE_MODE_SINGLE:
       $("#councils").hide();
       awaitParseSingleMinute(MODE_SINGLE_MINUTE_PARSE);
       break;
